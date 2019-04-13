3502970eefc906098318a2d680d3ca2fe98f013d
sasha <sashazemzer@gmail.com>
2019-04-13 20:00:31 +0300

    ура кодирует наконец то

--- src/main/java/org/donntu/knt/mksit/lab2/Main.java
+++ src/main/java/org/donntu/knt/mksit/lab2/Main.java
@@ -10,15 +10,15 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
 
-
+        //TODO: обрезать буффер по размеру файла. когда инициализируется буфер просто удалять все ненужное
         //LZ77 lz = new LZ77(30,30);
-        LZ77ByteEdition lz = new LZ77ByteEdition(30,30);
-        /*String filename = "files/file.txt";
+        LZ77ByteEdition lz = new LZ77ByteEdition(150,150);
+        String filename = "files/file.txt";
         String compressedFilename = "files/file.txt.lz77";
-        String decompressedFilename = "files/dec_file.txt";*/
-        String filename = "files/image.bmp";
+        String decompressedFilename = "files/dec_file.txt";
+        /*String filename = "files/image2.bmp";
         String compressedFilename = "files/image.bmp.lz77";
-        String decompressedFilename = "files/dec_image.bmp";
+        String decompressedFilename = "files/dec_image.bmp";*/
         try {
             lz.compress(filename, compressedFilename);
             lz.decompress(compressedFilename, decompressedFilename);
